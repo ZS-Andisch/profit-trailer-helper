8156c75f0e2badd2ec8cd58287a8a32201c69546
Andreas Schneider <andreas@zweischneider.de>
2018-02-10 10:44:22 +0100

    - init

---
+++ ProfitTrailer-autorestart.cmd
@@ -0,0 +1,27 @@
+:: Change restart time at line 16
+
+@echo off
+
+:loop
+echo ######### START APPLICATION %time:~-11,2%:%time:~-8,2%:%time:~-5,2%  - %date:~-4%-%date:~-7,2%-%date:~-10,2% #########
+echo %time:~-11,2%:%time:~-8,2%:%time:~-5,2% - start Profit Trailer
+start "ProfitTrailer" java -jar ProfitTrailer.jar -XX:+UseConcMarkSweepGC -Xmx512m -Xms512m
+
+timeout /t 20 >null
+echo.
+echo ######### CLOSE BROWSER TAB %time:~-11,2%:%time:~-8,2%:%time:~-5,2%  - %date:~-4%-%date:~-7,2%-%date:~-10,2% #########
+taskkill /F /FI "WindowTitle eq ProfitTrailer Login*" /T >null
+
+:: First close Browser and than wait 60 seconds * 60 minutes * 4 houres = 10800 - first 25 timeout seconds = 10775
+timeout /t 10775 >null
+
+echo.
+echo ######### CLOSE APPLICATION %time:~-11,2%:%time:~-8,2%:%time:~-5,2%  - %date:~-4%-%date:~-7,2%-%date:~-10,2% #########
+echo %time:~-11,2%:%time:~-8,2%:%time:~-5,2% - close Java Application
+taskkill /IM java.exe /F >null
+echo %time:~-11,2%:%time:~-8,2%:%time:~-5,2% - close Profit Trailer
+taskkill /F /FI "WindowTitle eq "Profit*"" /T >null
+timeout /t 7 >null
+echo.
+echo.
+goto loop
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # profit-trailer-helper
-Some helper functions like autorestart of ProfitTrailer every x houres.
+Some helper functions for ProfitTrailer
+- Automatically restart every x houres to work around freezes
+- Automatically close browser tab after start ProfitTrailer
+
+## ProfitTrailer-autostart.cmd
+_This function actually works only on Windows installations._
+
+Sometimes ProfitTrailer freezes. With this script you can work around this problem and you do not have to worry anymore. In addition, it also closes the browser tab automatically.
